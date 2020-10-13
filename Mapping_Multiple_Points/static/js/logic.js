6f0d7ec795fcfcf2af8c96244e507a102b51c504
Shayna Jefferson <shaynajj@gmail.com>
2020-10-13 18:25:59 -0400

    update docs

--- Mapping_Multiple_Points/static/js/logic.js
+++ Mapping_Multiple_Points/static/js/logic.js
@@ -30,12 +30,99 @@ let map = L.map('mapid').setView([40.7, -94.5], 4);
 //     radius: 300
 // }).addTo(map);
 
-//  Change marker using circleMarker function. Measures the radius in pixels
-L.circleMarker([34.0522, -118.2437], {
-    radius: 300,
-    color: 'black',
-    fillColor: '#ffffa1', 
-}).addTo(map);
+// //  Change marker using circleMarker function. Measures the radius in pixels, which gives a much bigger size
+// L.circleMarker([34.0522, -118.2437], {
+//     radius: 300,
+//     color: 'black',
+//     fillColor: '#ffffa1', 
+// }).addTo(map);
+
+// // Mapping Multiple Points
+// // An array containing each city's location, state, and population.
+// let cities = [{
+//     location: [40.7128, -74.0059],
+//     city: "New York City",
+//     state: "NY",
+//     population: 8398748
+//   },
+//   {
+//     location: [41.8781, -87.6298],
+//     city: "Chicago",
+//     state: "IL",
+//     population: 2705994
+//   },
+//   {
+//     location: [29.7604, -95.3698],
+//     city: "Houston",
+//     state: "TX",
+//     population: 2325502
+//   },
+//   {
+//     location: [34.0522, -118.2437],
+//     city: "Los Angeles",
+//     state: "CA",
+//     population: 3990456
+//   },
+//   {
+//     location: [33.4484, -112.0740],
+//     city: "Phoenix",
+//     state: "AZ",
+//     population: 1660272
+//   }
+//   ];
+
+// // Loop through the cities array and create one marker for each city.
+// cities.forEach(function(city) {
+//     console.log(city)
+//     L.marker(city.location).addTo(map);
+// });
+
+// Move data to separate file, useful for larger data sets
+// Get data from cities.js
+let cityData = cities;
+
+// // Loop through the cities array and create one marker for each city.
+// cityData.forEach(function(city) {
+//     console.log(city)
+//     L.marker(city.location).addTo(map);
+// });
+
+// // Re-write code to add popup to each marker
+// cityData.forEach(function(city) {
+//     console.log(city)
+//     L.marker(city.location)
+//     .bindPopup("<h2>" + city.city + ", " + city.state + "</h2> <hr> <h3>Population " + city.population + "</h3>")
+//   .addTo(map);
+// });
+
+// // Re-write code to format population with a thousands separator 
+// cityData.forEach(function(city) {
+//     console.log(city)
+//     L.marker(city.location)
+//     .bindPopup("<h2>" + city.city + ", " + city.state + "</h2> <hr> <h3>Population " + city.population.toLocaleString() + "</h3>")
+//   .addTo(map);
+// });
+
+// // Re-write code to change the marker for each city to a circle that has a radius equivalent to the city's population 
+// cityData.forEach(function(city) {
+//     console.log(city)
+//     L.circleMarker(city.location, {
+//         radius: city.population
+//     })
+//     .bindPopup("<h2>" + city.city + ", " + city.state + "</h2> <hr> <h3>Population " + city.population.toLocaleString() + "</h3>")
+//     .addTo(map);
+// });
+
+// Re-write code to change the marker for each city to a circle that has a radius equivalent to the city's population 
+// Must scale it down because the radii are too large to fit the map
+cityData.forEach(function(city) {
+    console.log(city)
+    L.circleMarker(city.location, {
+        radius: city.population/100000
+    })
+    .bindPopup("<h2>" + city.city + ", " + city.state + "</h2> <hr> <h3>Population " + city.population.toLocaleString() + "</h3>")
+    .addTo(map);
+});
 
 //   // We create the tile layer that will be the background of our map.
 // let streets = L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
